68be8d896fd60cc960d9633d53456680bd056636
DanielAristy <danielaristy22@gmail.com>
2021-07-02 01:37:08 -0500

    Estructura base del proyecto y configuracion de firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBGEEnma24zE7QX6_cH-3I0vZ5ncwWBLaQ",
+    authDomain: "fir-reactchat-e2abb.firebaseapp.com",
+    projectId: "fir-reactchat-e2abb",
+    storageBucket: "fir-reactchat-e2abb.appspot.com",
+    messagingSenderId: "211105915867",
+    appId: "1:211105915867:web:9a5b3b42b4daa033d87dfe",
+    measurementId: "G-G00J5GRKSH"
+};
+firebase.initializeApp(config);
+export const auth = firebase.auth;
+export const db = firebase.database();

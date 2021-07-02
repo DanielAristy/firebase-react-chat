4f10010dacf22310c1802ceb668e6c1f6eb8a418
DanielAristy <danielaristy22@gmail.com>
2021-07-02 13:56:32 -0500

    Dandole estilo al home de la app con Boostrap

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,17 +6,16 @@ import { Link } from 'react-router-dom';
 export default class HomePage extends Component {
   render() {
     return (
-      <div lass="container">
+      <div className="home">
         <Header></Header>
         <section>
-          <div class="jumbotron">
-            <div>
-              <h1 >Welcome to Chatty</h1>
-              <p>A great place to share your thoughts with friends</p>
-              <div>
-                <Link to="/signup">Create New Account</Link>
-                <br />
-                <Link to="/login">Login to Your Account</Link>
+          <div className="jumbotron jumbotron-fluid py-5">
+            <div className="container text-center py-5">
+              <h1 className="display-4">Welcome to Chatty</h1>
+              <p className="lead">A great place to share your thoughts with friends</p>
+              <div className="mt-4">
+                <Link className="btn btn-primary px-5 mr-3" to="/signup">Create New Account</Link>
+                <Link className="btn px-5" to="/login">Login to Your Account</Link>
               </div>
             </div>
           </div>
